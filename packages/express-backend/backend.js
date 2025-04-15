df9edf2c29f5410c35a511ac406f6dde959f8473
Ivan Martinez <eyemart@Mac.lan>
2025-04-15 13:17:55 -0700

    working find with id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,19 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const findUserById = (id) => 
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined){
+        res.status(404).send("Resource not found.");
+    } else{
+        res.send(result);
+    }
+});
+
 const findUserByName = (name) => {
     return users["users_list"].filter(
         (user) => user["name"] === name
