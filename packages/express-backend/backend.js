ddf1a14dbb247c96bd4b337a0602d3a12ba82c54
Ivan Martinez <eyemart@Ivans-MacBook-Air.local>
2025-04-15 17:07:43 -0700

    working post

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,25 +3,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-const findUserById = (id) => 
-    users["users_list"].find((user) => user["id"] === id);
-
-app.get("/users/:id", (req, res) => {
-    const id = req.params["id"];
-    let result = findUserById(id);
-    if (result === undefined){
-        res.status(404).send("Resource not found.");
-    } else{
-        res.send(result);
-    }
-});
-
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-};
-
 const users = {
     users_list: [
       {
@@ -72,3 +53,35 @@ app.get("/users", (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    console.log('received :', userToAdd);
+    addUser(userToAdd);
+    res.send();
+
+});
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined){
+        res.status(404).send("Resource not found.");
+    } else{
+        res.send(result);
+    }
+});
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
