e8e29f6344d748c8e770739859d4e49aba18739c
Ivan Martinez <eyemart@Mac.lan>
2025-04-27 00:36:46 -0700

    working integration with database

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,17 @@
 import express from "express";
 import cors from "cors";
+import userService from "./services/user-service.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to Db "users"
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;
@@ -41,31 +53,18 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-const getNameAndJob = (name, job) =>{
-    return users["users_list"].filter(
-        (user) => (user.name === name) && (user.job) === job
-    );
-}
-
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-};
 
 app.get("/users", (req, res) => {
     const name = req.query.name;
     const job = req.query.job;
-    let result = users;
-    if (name != undefined && job != undefined){
-        result = getNameAndJob(name, job);
-        result = { users_list: result};
-    }
-    else if (name != undefined){
-        result = findUserByName(name);
-        result = { users_list: result};
-    }
-    res.send(result);
+    userService.getUsers(name, job)
+    .then((result) => {
+      result = { users_list: result};
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(port, () => {
@@ -83,9 +82,11 @@ const addUser = (user) => {
 
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
-    addUser(userToAdd);
-    res.status(201).send(userToAdd);
-
+    userService.addUser(userToAdd)
+    .then((result) => {
+      res.status(201).send(result);
+    })
+    .catch((error)=> console.log(error));
 });
 
 const findUserById = (id) =>
@@ -93,12 +94,16 @@ const findUserById = (id) =>
 
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"];
-    let result = findUserById(id);
-    if (result === undefined){
-        res.status(404).send("Resource not found.");
-    } else{
-        res.send(result);
-    }
+    userService.findUserById(id)
+    .then((result)=>{
+        if (result === undefined){
+            res.status(404).send("Resource not found.");
+        } else{
+            res.send(result);
+        }
+      }
+    )
+    .catch((error)=>console.log(error));
 });
 
 app.delete("/users/:id", (req, res) => {
